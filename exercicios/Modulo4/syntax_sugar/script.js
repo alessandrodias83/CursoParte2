6a5256b75deda71cb818ee10ca00964d916fd7ad
Alessandro <158523798+alessandrodias83@users.noreply.github.com>
2024-09-28 21:55:11 -0300

    atualizado projeto

--- exercicios/Modulo4/syntax_sugar/script.js
+++ exercicios/Modulo4/syntax_sugar/script.js
@@ -1,5 +1,17 @@
+ let pensamentos = [];
+
+let pensamento = prompt("Digite um pensamento:");
+
 function perguntarPensamentos() {
-  
+
+
+  while (qtde !== "fim") {
+    pensamentos.push(pensamento);
+    adicionarPensamento(pensamento);
+   pensamento = prompt("Digite um pensamento:")
+  }
+
+
 }
 
 function adicionarPensamento(pensamento) {
--- projetoArray/script.js
+++ projetoArray/script.js
@@ -1,6 +1,53 @@
 
+let contador = 3;
 
-const tarefas = [];
+function finalizarTarefa(elemento) {
+
+    const li = elemento.parentNode; //peguei o elemento pai do spam no caso o li, assim eu consigo colocar a nova classe em toda a linha
+
+
+    const estaFinalizada = li.classList.contains("finalizada");
+    if (estaFinalizada === true) {
+        contador++;
+    } else {
+        contador--;
+    }
+
+    li.classList.toggle("finalizada")
+    atualizarContador(); //criei uma função com o mesmo código que se repete, o contador, dessa forma, eu coloco o código em uma função e quando preciso usar, basta chamar a função.
+}
+
+
+function finalizarTodas() {
+    const tarefas = document.querySelectorAll("li");
+
+    let index = 0;
+    while(index < tarefas.length) {
+        tarefas[index].classList.add("finalizada");
+        index++;
+    }
+
+    contador = 0;
+    atualizarContador();
+    
+
+}
+
+function atualizarContador() {
+    const elementoContador = document.querySelector("h1"); // alterei o contador da tag h1
+    elementoContador.innerHTML = `To-do List(${contador})`
+}
+
+
+
+
+
+
+
+
+
+
+/*const tarefas = [];
 
 const qtde = Number(prompt("Quantas tarefas você quer adicionar?"));
 
@@ -12,20 +59,18 @@ while (qtde > contador) {
     //contador +=1; soma o que quiser, até string
     contador++; /*so aumenta 1 */
 
+/*
 }
-
 contador = 0;
 const lista = document.querySelector("ul");
 while(qtde > contador) {
-    lista.innerHTML += `<li>${tarefas[contador]}</li>`;
-    contador ++;
+lista.innerHTML += `<li>${tarefas[contador]}</li>`;
+contador ++;
 }
-
-
-
 
 
 /*
 if - roda o código se a condição for verdadeira - somente 1 vez
 while - roda enquanto a condição for verdadeira
- */
+*/
+
