9ce5014b2a8fccbe7cd7b1886d0a907a543ad14c
Alessandro <158523798+alessandrodias83@users.noreply.github.com>
2024-09-29 22:39:41 -0300

    melhorias no código

--- projetoArray/script.js
+++ projetoArray/script.js
@@ -1,5 +1,56 @@
 
-let contador = 3;
+let contador = 0;
+const tarefas = [
+    { titulo: "Adicione uma tarefa no botão acima ☝️", status: "progresso" },
+    { titulo: "Passe o mouse na tarefa para ver o botão excluir 🗑️", status: "progresso" },
+    { titulo: "Clique na tarefa para marca-la como feita ✔️", status: "progresso" },
+];
+
+//Objeto precisa ter: título e status
+
+function atualizarTarefas() {
+    const ul = document.querySelector("ul");
+    ul.innerHTML = ""; // antes de iniciar uma nova tarefa, eu limpo para não duplicar
+    contador = 0; // também preciso limpar o contador
+
+    for (let index = 0; index < tarefas.length; index++) {
+        let elementoTarefa;
+        if (tarefas[index].status === "finalizado") {
+           elementoTarefa = `
+        <li class="finalizada>
+            <div class="btn-delete">
+                    <ion-icon name="trash-outline"></ion-icon>
+                </div>
+                <span onclick="finalizarTarefa(this)">${tarefas[index].titulo}</span>
+            </li> 
+        `;
+            // dentro do objeto preciso indicar o que quero pegar, no caso, o título que foi passado na const tarefas
+        } else {
+            elementoTarefa = `
+        <li>
+            <div class="btn-delete">
+                    <ion-icon name="trash-outline"></ion-icon>
+                </div>
+                <span onclick="finalizarTarefa(this)">${tarefas[index].titulo}</span>
+            </li> 
+        `;
+        }
+        ul.innerHTML += elementoTarefa;
+        contador++; // adiciona o contador
+
+        atualizarContador(); // chamo a função aqui
+    }
+}
+
+atualizarTarefas();
+
+function adicionarNovaTarefa() {
+    const novaTarefa = document.querySelector("input").value; // devo usar .value para "pegar o texto dentro do input"
+    tarefas.push(novaTarefa);
+    atualizarTarefas();
+    document.querySelector("input").value = ""; // essa linha é para limpar o imput para receber uma nova lista
+}
+
 
 function finalizarTarefa(elemento) {
 
@@ -22,14 +73,14 @@ function finalizarTodas() {
     const tarefas = document.querySelectorAll("li");
 
     let index = 0;
-    while(index < tarefas.length) {
+    while (index < tarefas.length) {
         tarefas[index].classList.add("finalizada");
         index++;
     }
 
     contador = 0;
     atualizarContador();
-    
+
 
 }
 
